f5c4d1135c3e7bcfcd2bfb1ddff4b6edf45d3727
ShohMansurjonovich <shohruhramozonov2662@gmail.com>
2024-02-08 11:31:32 +0500

    changing postgresql

--- migration/01_create_data.up.sql
+++ migration/01_create_data.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE "roles"(
 
 CREATE TABLE "users"(
     "id" UUID PRIMARY KEY,
-    "role_id" UUID REFERENCES "roles" ("id"),
+    "role_id" UUID,
     "first_name" VARCHAR (50) NOT NULL,
     "last_name" VARCHAR(50) NOT NULL,
     "email" VARCHAR(100),
@@ -33,15 +33,15 @@ CREATE TABLE "courses"(
 
 CREATE TABLE "courseOfUsers"(
     "id" UUID PRIMARY KEY,
-    "user_id" UUID REFERENCES "users"("id"),
-    "course_id" UUID REFERENCES "courses"("id"),
+    "user_id" UUID ,
+    "course_id" UUID,
     "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP
 );
 
 CREATE TABLE "course_info"(
     "id" UUID PRIMARY KEY,
-    "course_id" UUID REFERENCES "courses"("id"),
+    "course_id" UUID ,
     "percent_of_done" NUMERIC NOT NULL,
     "remaining_exam" VARCHAR(20) NOT NULL,
     "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -50,7 +50,7 @@ CREATE TABLE "course_info"(
 
 CREATE TABLE "course_report"(
     "id" UUID PRIMARY KEY,
-    "course_id" UUID REFERENCES "courses"("id"),
+    "course_id" UUID,
     "students" NUMERIC NOT NULL,
     "type" VARCHAR(10) NOT NULL,
     "done_all" NUMERIC NOT NULL,
@@ -60,3 +60,15 @@ CREATE TABLE "course_report"(
     "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP
 );
+
+
+ALTER TABLE "users" ADD CONSTRAINT "ur_users_roles" FOREIGN KEY ("role_id") REFERENCES "roles"("id");
+
+ALTER TABLE "courseOfUsers" ADD CONSTRAINT "cu_course_users" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+
+ALTER TABLE "courseOfUsers" ADD CONSTRAINT "cu_users_course" FOREIGN KEY ("course_id") REFERENCES "courses"("id");
+
+ALTER TABLE "course_info" ADD CONSTRAINT "cic_info_course" FOREIGN KEY ("course_id") REFERENCES "courses"("id");
+
+ALTER TABLE "course_report" ADD CONSTRAINT "crc_report_course" FOREIGN KEY ("course_id") REFERENCES "courses"("id");
+
